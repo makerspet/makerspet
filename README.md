81c6358e2e3fd382b4990ff1ef8f69bb35bbbc01
Ilia O <iliao@makerspet.com>
2024-09-29 00:05:48 +0000

    Empty package

---
+++ README.md
@@ -0,0 +1,1 @@
+ROS2 Python wrapper for Maker's Pet robots
---
+++ setup.py
@@ -0,0 +1,34 @@
+from setuptools import find_packages, setup
+
+package_name = 'makerspet'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    author='Ilia O.',
+    author_email='iliao@makerspet.com',
+    maintainer='Ilia O.',
+    maintainer_email='iliao@makerspet.com',
+    keywords=['ROS'],
+    description='ROS2 Python wrapper for Maker''s Pet robots',
+    license='Apache-2.0',
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python',
+        'Topic :: Software Development',
+    ],
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
